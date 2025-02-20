bb33aedeba6bdcc7c4b57a3aac27d6396b121292
KamilSz <kamil.szamajda.programista@gmail.com>
2025-02-20 15:46:49 +0100

    PersonalBudgetApp files preset with code lines

--- PersonalBudgetApp.h
+++ PersonalBudgetApp.h
@@ -3,10 +3,38 @@
 
 #include <iostream>
 
+#include "UserManager.h"
+#include "BalanceManager.h"
+
 using namespace std;
 
 class PersonalBudgetApp {
+    UserManager userManager;
+    BalanceManager *balanceManager;
+    const string INCOME_FILE_NAME;
+    const string EXPENSE_FILE_NAME;
 
+public:
+    PersonalBudgetApp(string userFileName, string incomeFileName, string expenseFileName)
+        :/*userManager(userFileName),*/INCOME_FILE_NAME(incomeFileName), EXPENSE_FILE_NAME(expenseFileName){
+
+        balanceManager = NULL;
+    };
+    ~PersonalBudgetApp(){
+        delete balanceManager;
+        balanceManager = NULL;
+    };
+
+    void userLoggingIn();
+    void userRegistration();
+    void loggedInUserPasswordChange();
+    void userLogout();
+    void addIncome();
+    void addExpense();
+    void displayCurrenthMonthBalance();
+    void displayPreviousMonthBalance();
+    void displaySelectedPeriodBalanace();
+    bool isUserLoggedIn();
 
 };
 
